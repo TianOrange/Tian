f32ac379b51579dfd2272251ec843ff78fd7e796
TianOrange <983180102@qq.com>
2016-06-14 10:39:45 +0800

    Initial Commit

---
+++ ImageLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageLabel
+//
+//  Created by Tian on 16/6/14.
+//  Copyright © 2016年 Tian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
